6d3713c282b3aa02e3db18148daf37fca2b657b9
Bruce <1033684650@qq.com>
2023-04-05 22:06:17 +0800

    finish the login page

--- src/containers/Login/index.tsx
+++ src/containers/Login/index.tsx
@@ -1,16 +1,93 @@
-import React, { useState, useEffect } from 'react';
+import {
+  LockOutlined,
+  MobileOutlined,
+} from '@ant-design/icons';
+import {
+  LoginForm,
+  ProFormCaptcha,
+  ProFormCheckbox,
+  ProFormText,
+  ProConfigProvider,
+} from '@ant-design/pro-components';
+import { message, Tabs } from 'antd';
+import styles from './index.module.less';
 
-import style from './index.module.less';
 
-/**
-* 登录页面
-*/
-const Login = () => {
-    const [state, setState] = useState();
-    useEffect(() => {
-        console.log(state, setState);
-    }, []);
-    return (<div className={style.container}>sss</div>);
+export default () => {
+  return (
+    <ProConfigProvider hashed={false}>
+      <div className={styles.container}>
+        <LoginForm
+          logo="https://github.githubassets.com/images/modules/logos_page/Octocat.png"
+          title="Github"
+          subTitle="全球最大的代码托管平台"
+        >
+          <Tabs>
+            <Tabs.TabPane key={'phone'} tab={'手机号登录'} />
+          </Tabs>
+          
+              <ProFormText
+                fieldProps={{
+                  size: 'large',
+                  prefix: <MobileOutlined className={'prefixIcon'} />,
+                }}
+                name="mobile"
+                placeholder={'手机号'}
+                rules={[
+                  {
+                    required: true,
+                    message: '请输入手机号！',
+                  },
+                  {
+                    pattern: /^1\d{10}$/,
+                    message: '手机号格式错误！',
+                  },
+                ]}
+              />
+              <ProFormCaptcha
+                fieldProps={{
+                  size: 'large',
+                  prefix: <LockOutlined className={'prefixIcon'} />,
+                }}
+                captchaProps={{
+                  size: 'large',
+                }}
+                placeholder={'请输入验证码'}
+                captchaTextRender={(timing, count) => {
+                  if (timing) {
+                    return `${count} ${'获取验证码'}`;
+                  }
+                  return '获取验证码';
+                }}
+                name="captcha"
+                rules={[
+                  {
+                    required: true,
+                    message: '请输入验证码！',
+                  },
+                ]}
+                onGetCaptcha={async () => {
+                  message.success('获取验证码成功！验证码为：1234');
+                }}
+              />
+          <div
+            style={{
+              marginBlockEnd: 24,
+            }}
+          >
+            <ProFormCheckbox noStyle name="autoLogin">
+              自动登录
+            </ProFormCheckbox>
+            <a
+              style={{
+                float: 'right',
+              }}
+            >
+              忘记密码
+            </a>
+          </div>
+        </LoginForm>
+      </div>
+    </ProConfigProvider>
+  );
 };
-
-export default Login;
