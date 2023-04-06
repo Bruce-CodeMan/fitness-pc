28248b765c613ce0a20d3740b709f518c55b8497
Bruce <1033684650@qq.com>
2023-04-06 22:22:36 +0800

    remove App.tsx and App.css

---
+++ src/containers/Home/index.tsx
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from 'react';
+
+import style from './index.module.less';
+
+/**
+*   首页
+*/
+const Home = ({}) => {
+    const [state, setState] = useState();
+    useEffect(() => {
+        console.log(state, setState);
+    },[]);
+    return (<div className={style.container}>首页</div>);
+};
+
+export default Home;    
+
+
--- src/main.tsx
+++ src/main.tsx
@@ -4,17 +4,19 @@
  * @Description: 
  */
 import ReactDOM from 'react-dom/client'
-import Login from './containers/Login';
 import './index.css'
 import { ApolloProvider } from '@apollo/client';
 import { client } from './utils/apollo';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { ROUTE_CONFIG } from './routes';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <ApolloProvider client={client}>
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={ <Login />}/>
+        { ROUTE_CONFIG.map((item) => 
+          <Route path={item.path} key={item.key} element={<item.element/>}/>
+          )}
       </Routes>
     </BrowserRouter>
   </ApolloProvider>
