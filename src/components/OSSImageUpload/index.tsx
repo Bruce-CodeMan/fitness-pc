6047a3b8f795d1aab11104a1831044435be979cf
Bruce-CodeMan <1033684650@qq.com>
2023-04-13 09:30:26 +0800

    OSSImageUpload component

---
+++ src/components/OSSImageUpload/index.tsx
@@ -0,0 +1,10 @@
+/*
+ * @Date: 2023-04-13 09:28:49
+ * @Author: Bruce
+ * @Description: 
+ */
+const OSSImageUpload = () => {
+    
+}
+
+export default OSSImageUpload;
--- src/containers/My/index.tsx
+++ src/containers/My/index.tsx
@@ -3,12 +3,29 @@
  * @Author: Bruce
  * @Description: 
  */
+import { ProForm, ProFormText, ProFormTextArea } from "@ant-design/pro-components";
+import { Row, Col } from "antd";
 
 /**
 *   首页
 */
 const My = ({}) => {
-    return (<div>my</div>);
+    return (
+        <div>
+            <ProForm>
+                <Row>
+                    <Col>
+                        <ProFormText name="tel" label="手机号" tooltip="不能修改" disabled/>
+                        <ProFormText name="name" label="昵称" placeholder="请输入昵称"/>
+                        <ProFormTextArea name="desc" label="简介" placeholder="请输入简介"/>
+                    </Col>
+                    <Col>
+                        <ProFormText />
+                    </Col>
+                </Row>
+            </ProForm>
+        </div>
+    );
 };
 
 export default My;    
