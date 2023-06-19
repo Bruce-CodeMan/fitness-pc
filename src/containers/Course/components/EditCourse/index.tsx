16f5071d52cb8fe390b4361b5c7662d8d91430e6
Bruce <1033684650@qq.com>
2023-06-20 00:09:04 +0800

    finish the EditCourse index.tsx

--- src/containers/Course/components/EditCourse/index.tsx
+++ src/containers/Course/components/EditCourse/index.tsx
@@ -1,5 +1,6 @@
-import { Drawer } from "antd"
+import { Drawer, Form, Input, InputNumber, Row, Col, Space, Button } from "antd"
 
+const { TextArea } = Input
 interface IProps {
     id?: string;
     onClose: () => void;
@@ -11,9 +12,71 @@ const EditCourse = ({
     onClose,
     open
 }: IProps) => {
+    const [ form ] = Form.useForm()
+    const onSubmitHandler = async () => {
+        const data = await form.validateFields();
+        console.log("data: ", data)
+    }
+
     return (
-        <Drawer open={open} onClose={onClose}/>
+        <Drawer 
+            title={id?"编辑课程":"新建课程"}
+            width={720}
+            open={open} 
+            onClose={onClose}
+            extra={(
+                <Space>
+                    <Button onClick={onClose}>取消</Button>
+                    <Button onClick={onSubmitHandler} type="primary">提交</Button>
+                </Space>
+            )}
+        >
+            <Form form={form}>
+                <Form.Item label="课程名称" name="name" rules={[{ required: true }]}>
+                    <Input />
+                </Form.Item>
+                
+                <Form.Item label="适龄人群" name="group" rules={[{ required: true }]}>
+                    <Input />
+                </Form.Item>
+                <Row gutter={20}>
+                    <Col>
+                        <Form.Item label="限制人数" name="limitNumber" rules={[{ required: true }]}>
+                            <InputNumber min={0} addonAfter="人"/>
+                        </Form.Item>
+                    </Col>
+                    <Col>
+                        <Form.Item label="持续时长" name="duration" rules={[{ required: true }]}>
+                            <InputNumber min={0} addonAfter="分钟"/>
+                        </Form.Item>
+                    </Col>
+                    
+                </Row>
+                
+                <Form.Item label="基础能力" name="baseAbility" rules={[{ required: true }]}>
+                    <Input />
+                </Form.Item>
+                <Form.Item label="预约信息" name="reserveInfo" rules={[{ required: true }]}>
+                    <TextArea rows={5} showCount maxLength={200}/>
+                </Form.Item>
+                <Form.Item label="课程描述" name="desc" rules={[{ required: true }]}>
+                    <TextArea rows={5} showCount maxLength={200}/>
+                </Form.Item>
+                
+                <Form.Item label="退款信息" name="refundInfo" rules={[{ required: true }]}>
+                    <TextArea rows={5} showCount maxLength={200}/>
+                </Form.Item>
+                
+                <Form.Item label="其他信息" name="otherInfo">
+                    <TextArea rows={5} showCount maxLength={200}/>
+                </Form.Item>
+            </Form>
+        </Drawer>
     )
+}
+
+EditCourse.defaultProps = {
+    id: ''
 }
 
 export default EditCourse;
